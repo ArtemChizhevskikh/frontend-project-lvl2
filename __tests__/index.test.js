4ae516843af5ccea1f226c5b51ad5798747004a3
ArtemChizevskikh <artem74ru@hotmail.com>
2020-04-01 19:01:59 +0500

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,19 @@
 import path from 'path';
 import genDiff from '../src/index.js';
 
-test('plain compare', () => {
-  const path1 = path.resolve(__dirname, '..', '__fixtures__', 'before.json');
-  const path2 = path.resolve(__dirname, '..', '__fixtures__', 'after.json');
+const extensions = [['json'], ['yml'], ['ini']];
+const expected = ['{',
+  '    host: hexlet.io',
+  '  - timeout: 50',
+  '  + timeout: 20',
+  '  - proxy: 123.234.53.22',
+  '  - follow: false',
+  '  + verbose: true',
+  '}'];
 
-  const expected = ['{',
-    '  host: hexlet.io',
-    '- timeout: 50',
-    '+ timeout: 20',
-    '- proxy: 123.234.53.22',
-    '- follow: false',
-    '+ verbose: true',
-    '}'];
+test.each(extensions)('plain %s compare', (extension) => {
+  const path1 = path.resolve(__dirname, 'fixtures', `before.${extension}`);
+  const path2 = path.resolve(__dirname, 'fixtures', `after.${extension}`);
 
   expect(genDiff(path1, path2)).toBe(expected.join('\n'));
 });
