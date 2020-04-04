235e203f9811934f1bf8e61b9f7f741e468bf016
ArtemChizevskikh <artem74ru@hotmail.com>
2020-04-04 23:08:50 +0500

    reworked

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import _ from 'lodash';
+import { has, isObject, flatten } from 'lodash';
 import getParser from './parsers.js';
 
 const parseFile = (pathToFile) => {
@@ -10,21 +10,68 @@ const parseFile = (pathToFile) => {
   return parser(fs.readFileSync(fullPath, 'utf-8'));
 };
 
-const genDiff = (path1, path2) => {
-  const file1 = parseFile(path1);
-  const file2 = parseFile(path2);
-  const keys = Object.keys({ ...file1, ...file2 });
-  const result = keys.reduce((acc, key) => {
-    if (_.has(file1, key)) {
-      if (_.has(file2, key)) {
-        return (file1[key] === file2[key]) ? `${acc},    ${key}: ${file1[key]}`
-          : `${acc},  - ${key}: ${file1[key]},  + ${key}: ${file2[key]}`;
-      }
-      return `${acc},  - ${key}: ${file1[key]}`;
-    }
-    return `${acc},  + ${key}: ${file2[key]}`;
-  }, '');
-  return `{${result},}`.split(',').join('\n');
+const makeIndent = (depth) => '    '.repeat(depth);
+
+const processKey = (node, depth) => {
+  const indentLength = makeIndent(depth).length;
+  switch (node.status) {
+    case 'unchanged':
+      return `${makeIndent(depth)}${node.name}`;
+    case 'removed':
+      return `${'- '.padStart(indentLength)}${node.name}`;
+    case 'added':
+      return `${'+ '.padStart(indentLength)}${node.name}`;
+    default:
+      throw new Error(`Unknown statue ${node.status}!`);
+  }
 };
 
-export default genDiff;
+const processValue = (value, depth) => {
+  if (isObject(value)) {
+    const result = Object.entries(value)
+      .map(([k, v]) => `{\n${makeIndent(depth + 1)}${k}: ${v}\n${makeIndent(depth)}}`);
+    return flatten(result);
+  }
+  return `${value}`;
+};
+
+const stringify = (object, depth) => {
+  const indent = '    '.repeat(depth);
+  return object.reduce((acc, node) => {
+    if (has(node, 'children')) {
+      return [...acc, `${indent}${node.name}: {`, flatten(stringify(node.children, depth + 1)).join('\n'), `${indent}}`];
+    }
+    return [...acc, `${processKey(node, depth)}: ${processValue(node.value, depth)}`];
+  }, []);
+};
+
+const iterAst = (node1, node2) => {
+  const keys = Object.keys({ ...node1, ...node2 }).sort();
+  return keys.map((key) => {
+    if (has(node1, key)) {
+      if (has(node2, key)) {
+        if (isObject(node1[key]) && isObject(node2[key])) {
+          return { name: key, children: flatten(iterAst(node1[key], node2[key])), status: 'unchanged' };
+        }
+        return (node1[key] === node2[key]) ? { name: key, value: node1[key], status: 'unchanged' }
+          : [{ name: key, value: node1[key], status: 'removed' }, { name: key, value: node2[key], status: 'added' }];
+      }
+      return { name: key, value: node1[key], status: 'removed' };
+    }
+    return { name: key, value: node2[key], status: 'added' };
+  });
+};
+
+
+const genDiff = (path1, path2) => {
+  const node1 = parseFile(path1);
+  const node2 = parseFile(path2);
+  return iterAst(node1, node2);
+};
+
+const render = (path1, path2) => {
+  const result = flatten(genDiff(path1, path2));
+  return `{\n${(stringify(result, 1)).join('\n')}\n}`;
+};
+
+export default render;
