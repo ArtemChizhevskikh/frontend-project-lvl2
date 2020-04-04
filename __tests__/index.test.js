1400e7b51706dab7d870a4f182928bb91d87813d
ArtemChizevskikh <artem74ru@hotmail.com>
2020-04-04 23:08:03 +0500

    modified

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,16 @@
 import path from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const extensions = [['json'], ['yml'], ['ini']];
-const expected = ['{',
-  '    host: hexlet.io',
-  '  - timeout: 50',
-  '  + timeout: 20',
-  '  - proxy: 123.234.53.22',
-  '  - follow: false',
-  '  + verbose: true',
-  '}'];
+
+const extensions = [['.json'], ['.yml'], ['.ini']];
+const getFullPath = (file, extension = '') => path.resolve(__dirname, 'fixtures',
+  `${file}${extension}`);
 
 test.each(extensions)('plain %s compare', (extension) => {
-  const path1 = path.resolve(__dirname, 'fixtures', `before.${extension}`);
-  const path2 = path.resolve(__dirname, 'fixtures', `after.${extension}`);
+  const path1 = getFullPath('before', extension);
+  const path2 = getFullPath('after', extension);
+  const expected = fs.readFileSync(getFullPath('expected'), 'utf-8');
 
-  expect(genDiff(path1, path2)).toBe(expected.join('\n'));
+  expect(genDiff(path1, path2)).toBe(expected);
 });
