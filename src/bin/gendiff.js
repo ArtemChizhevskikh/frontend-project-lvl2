f5bdc2ebe440c4bb26ef63750bfaac12a095990c
ArtemChizevskikh <artem74ru@hotmail.com>
2020-04-06 20:35:40 +0500

    add ability to choose output format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,11 +5,11 @@ import genDiff from '../index.js';
 
 program
   .usage('[options] <firstConfig> <secondConfig>')
-  .version('0.0.1')
+  .version('0.0.3')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
-  .arguments('<path1> <path2> [env]')
+  .option('-f, --format [type]', 'output format', 'string')
+  .arguments('<path1> <path2>')
   .action((path1, path2) => {
-    console.log(genDiff(path1, path2));
+    console.log(genDiff(path1, path2, program.format));
   })
   .parse(process.argv);
