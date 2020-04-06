d1ab8db31901975f4fb51c711b48ab3e91740536
ArtemChizhevskikh <50134327+ArtemChizhevskikh@users.noreply.github.com>
2020-04-06 20:40:53 +0500

    Revert "creater"
    This reverts commit 294df0ea9ec8426ede0bb15791b9997d9c02370f.

--- src/formatters/index.js
+++
@@ -1,9 +0,0 @@
-import toPlainText from './plain.js';
-import stringify from './stringify.js';
-
-const formatters = {
-  string: stringify,
-  plain: toPlainText,
-};
-
-export default (format) => formatters[format];
