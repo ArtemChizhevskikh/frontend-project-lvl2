8094d4ebd8486ec49164a977f2354cfbefabd708
ArtemChizevskikh <artem74ru@hotmail.com>
2020-04-06 20:34:31 +0500

    add test for new format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,13 +4,20 @@ import genDiff from '../src/index.js';
 
 
 const extensions = [['.json'], ['.yml'], ['.ini']];
-const getFullPath = (file, extension = '') => path.resolve(__dirname, 'fixtures',
-  `${file}${extension}`);
+const getFullPath = (file, extension = '') => path.resolve(__dirname, 'fixtures', `${file}${extension}`);
 
-test.each(extensions)('plain %s compare', (extension) => {
+test.each(extensions)('compare %s files', (extension) => {
   const path1 = getFullPath('before', extension);
   const path2 = getFullPath('after', extension);
   const expected = fs.readFileSync(getFullPath('expected'), 'utf-8');
 
-  expect(genDiff(path1, path2)).toBe(expected);
+  expect(genDiff(path1, path2, 'string')).toBe(expected);
+});
+
+test.each(extensions)('plain format', (extension) => {
+  const path1 = getFullPath('before', extension);
+  const path2 = getFullPath('after', extension);
+  const expected = fs.readFileSync(getFullPath('expectedPlain'), 'utf-8');
+
+  expect(genDiff(path1, path2, 'plain')).toBe(expected);
 });
