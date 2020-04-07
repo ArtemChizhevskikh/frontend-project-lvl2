9123021801fe0c1ae99da39b0a4718e92734b0d8
ArtemChizevskikh <artem74ru@hotmail.com>
2020-04-07 21:10:24 +0500

    add tests for json output

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,21 +3,16 @@ import fs from 'fs';
 import genDiff from '../src/index.js';
 
 
-const extensions = [['.json'], ['.yml'], ['.ini']];
-const getFullPath = (file, extension = '') => path.resolve(__dirname, 'fixtures', `${file}${extension}`);
+const extensions = [['.json', 'treelike'], ['.yml', 'treelike'], ['.ini', 'treelike'],
+  ['.json', 'plain'], ['.yml', 'plain'], ['.ini', 'plain'],
+  ['.json', 'json'], ['.yml', 'json'], ['.ini', 'json']];
 
-test.each(extensions)('compare %s files', (extension) => {
+const getFullPath = (file, extension = '.txt') => path.resolve(__dirname, 'fixtures', `${file}${extension}`);
+
+test.each(extensions)('compare %s files in %s format', (extension, format) => {
   const path1 = getFullPath('before', extension);
   const path2 = getFullPath('after', extension);
-  const expected = fs.readFileSync(getFullPath('expected'), 'utf-8');
+  const expected = fs.readFileSync(getFullPath(`${format}Expected`), 'utf-8');
 
-  expect(genDiff(path1, path2, 'string')).toBe(expected);
-});
-
-test.each(extensions)('plain format', (extension) => {
-  const path1 = getFullPath('before', extension);
-  const path2 = getFullPath('after', extension);
-  const expected = fs.readFileSync(getFullPath('expectedPlain'), 'utf-8');
-
-  expect(genDiff(path1, path2, 'plain')).toBe(expected);
+  expect(genDiff(path1, path2, format)).toBe(expected);
 });
